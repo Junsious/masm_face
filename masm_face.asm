0f1bb374ae1d47197455d86d70686e7fdc17f7a3
kqueej <alexandrd542@gmail.com>
2025-05-30 22:11:32 +0300

    Update masm_face.asm

--- masm_face.asm
+++ masm_face.asm
@@ -1,20 +1,36 @@
-CSEG segment
+CSEG segment 
+assume CS:CSEG, DS:CSEG, ES:CSEG, SS:CSEG
 org 100h
-Begin:
+
+Start:
     mov ax,0b800h
     mov es,ax
-    mov di,0
     mov al,1
     mov ah,31
-    mov cx,2000
+    mov cx,254
 
+Next_screen:
+    mov di,0
+    call Out_chars
+    inc al
+    loop Next_screen
+
+    mov ah,10h
+    int 16h
+
+    int 20h
+
+Out_chars proc
+    mov dx,cx
+    mov cx,2000
 Next_face:
     mov es:[di],ax
     add di,2
     loop Next_face
 
-    mov ah,10h
-    int 16h
-    int 20h
+    mov cx,dx
+    ret
+Out_chars endp
+
 CSEG ends
-end Begin
+end Start
